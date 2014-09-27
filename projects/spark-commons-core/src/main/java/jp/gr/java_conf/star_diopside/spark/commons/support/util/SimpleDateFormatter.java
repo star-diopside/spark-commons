6752ebd83854e0b6768fea3af1095af8d4396f4c
se-yoshi <se.yoshi@gmail.com>
2014-09-27 17:15:02 +0900

    SimpleDateFormatter のコンストラクタを修正

--- projects/spark-commons-core/src/main/java/jp/gr/java_conf/star_diopside/spark/commons/support/util/SimpleDateFormatter.java
+++ projects/spark-commons-core/src/main/java/jp/gr/java_conf/star_diopside/spark/commons/support/util/SimpleDateFormatter.java
@@ -3,7 +3,6 @@ package jp.gr.java_conf.star_diopside.spark.commons.support.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.function.Supplier;
 
 /**
  * スレッドローカルで保持される {@link SimpleDateFormat} に処理を委譲するクラス
@@ -11,22 +10,21 @@ import java.util.function.Supplier;
 public class SimpleDateFormatter {
 
     /** <code>yyyyMMdd</code> 形式の時刻値を扱う {@link SimpleDateFormatter} */
-    public static final SimpleDateFormatter BASIC_ISO_DATE = new SimpleDateFormatter(() -> new SimpleDateFormat(
-            "yyyyMMdd"));
+    public static final SimpleDateFormatter BASIC_ISO_DATE = new SimpleDateFormatter("yyyyMMdd");
 
     /** <code>yyyy-MM-dd</code> 形式の時刻値を扱う {@link SimpleDateFormatter} */
-    public static final SimpleDateFormatter ISO_LOCAL_DATE = new SimpleDateFormatter(() -> new SimpleDateFormat(
-            "yyyy-MM-dd"));
+    public static final SimpleDateFormatter ISO_LOCAL_DATE = new SimpleDateFormatter("yyyy-MM-dd");
 
+    /** スレッドローカル領域 */
     private ThreadLocal<SimpleDateFormat> formatter;
 
     /**
      * コンストラクタ
      * 
-     * @param supplier {@link SimpleDateFormatter}を生成する{@link Supplier}
+     * @param pattern 日付時刻文字列のフォーマットパターン
      */
-    public SimpleDateFormatter(Supplier<SimpleDateFormat> supplier) {
-        formatter = ThreadLocal.withInitial(supplier);
+    public SimpleDateFormatter(String pattern) {
+        formatter = ThreadLocal.withInitial(() -> new SimpleDateFormat(pattern));
     }
 
     /**
